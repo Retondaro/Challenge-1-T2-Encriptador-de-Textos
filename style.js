6422869cb732bbf6466a4acee2ad7b768bb520db
Retondaro <102809116+Retondaro@users.noreply.github.com>
2022-08-16 15:00:22 -0300

    Add files via upload
    Initial release

---
+++ style.js
@@ -0,0 +1,41 @@
+
+function encriptar(){
+var texto = document.getElementById("inputTexto").value.toLowerCase();
+var textoCifrado = texto.replace(/e/igm,"enter");
+var textoCifrado = textoCifrado.replace(/o/igm,"ober");
+var textoCifrado = textoCifrado.replace(/i/igm,"imes");
+var textoCifrado = textoCifrado.replace(/a/igm,"ai");
+var textoCifrado = textoCifrado.replace(/u/igm,"ufat");
+
+document.getElementById("imgboneco").style.display = "none";
+document.getElementById("texto").style.display = "none";
+document.getElementById("texto2").innerHTML = textoCifrado;
+document.getElementById("copiar").style.display = "show";
+document.getElementById("copiar").style.display = "inherit";
+}
+
+function desencriptar(){
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var textoCifrado = texto.replace(/enter/igm,"e");
+    var textoCifrado = textoCifrado.replace(/ober/igm,"o");
+    var textoCifrado = textoCifrado.replace(/imes/igm,"i");
+    var textoCifrado = textoCifrado.replace(/ai/igm,"a");
+    var textoCifrado = textoCifrado.replace(/ufat/igm,"u");
+    
+    document.getElementById("imgboneco").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = textoCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+    }
+
+function copiar(){
+    var container = document.querySelector("#texto2");
+    container.select();
+    document.execCommand("copy");
+    alert("Documento Copiado!")
+}
+
+
+       
+
